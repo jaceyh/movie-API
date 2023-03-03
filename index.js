3568dc2057cf179dd15a113c036b785c31c72e19
jaceyh <jacey.hallock@gmail.com>
2023-03-03 10:41:48 -0500

    add models.js and mongoose query functions

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const express = require ('express'),
   path = require('path'),
   bodyParser = require('body-parser'),
   uuid = require('uuid'),
-  morgan = require('morgan');
-  
+  morgan = require('morgan'),
+  mongoose = require('mongoose'),
+  Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Tags = Models.Tag;
+const Directors = Models.Director
+
 const app = express();
 
 app.use(bodyParser.json());
+
+//allow mongoose to connect to [cfDB]
+mongoose.connect('mongodb://localhost:27017/[cfDB]', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // create a write stream (in append mode)
 // a ‘log.txt’ file is created in root directory
@@ -16,6 +26,7 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {f
 // setup the logger
 app.use(morgan('combined', {stream: accessLogStream}));
 
+/*
 let movies = [
     {
       title: 'Free Solo',
@@ -69,7 +80,6 @@ let movies = [
     },
   ];
 
-
 let users = [
   {
     id: "1",
@@ -84,7 +94,7 @@ let users = [
     watchlistMovies: [],
   }
 ];
-
+*/
 
 // GET requests
 app.get('/', (req, res) => {
@@ -112,20 +122,70 @@ app.get('/movies/tags/:tags', (req, res) => {
   
 app.use('/documentation.html', express.static('public/documentation.html'));
 
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 
 //POST requests
-app.post('/user', (req, res) => {
-  //res.send('You want to create an account.  Great!  Idk how to code that yet.')});
-const newUser = req.body;
-  if (!newUser.username === '') {
-    const message = 'Missing "username" in request body';
-    res.status(400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  }
+
+//Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+        .create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        })
+        .then((user) =>{res.status(201).json(user)})
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error:' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
+
+
+
 
 
 //DELETE user account request
@@ -144,6 +204,39 @@ app.delete('/user/delete/:id', (req, res) => {
 
 
 //PUT requests (add to favorites, add to watchlist, UPDATE user info)
+
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+/*
 app.put('/user/:id', (req, res) => {
   const { id } = req.params;
   const updatedUser = req.body;
@@ -177,6 +270,7 @@ app.put('/user/:id/watchlist/:title', (req, res) => {
     res.status(200).send('Movie was added to your watchlist.');
   }
 );
+*/
 
 //DELETE Remove from favorites or watchlist
 app.delete('/user/:id/favorites/delete/:title', (req, res) => {
--- models.js
+++ models.js
@@ -10,3 +10,32 @@ let movieSchema = mongoose.Schema({
     Description: {type: String, required: true},
     Featured: {type: Boolean}
 });
+
+let tagSchema = mongoose.Schema({
+    Name: {type: String},
+    Description: {type: String}
+})
+
+let dirSchema = mongoose.Schema({
+    Name: {type: String},
+    Bio: {type: String}
+})
+
+
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+let Tag = mongoose.model('Tag', tagSchema);
+let Director = mongoose.model('Director', dirSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+module.exports.Tag = Tag;
+module.exports.Director = Director;
