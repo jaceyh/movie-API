11a80af0457f32a81d9e6d6e34abd6787bae73bb
jaceyh <jacey.hallock@gmail.com>
2023-04-28 09:35:40 -0400

    add authentication to /movies

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
   });
 
 //GET Movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(200).json(movies);
