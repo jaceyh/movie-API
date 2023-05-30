27253be985eb0594e4a290efb299f9856eaec640
jaceyh <jacey.hallock@gmail.com>
2023-05-30 07:59:00 -0400

    fix JSON

--- index.js
+++ index.js
@@ -84,7 +84,6 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) 
                 'preserveNullAndEmptyArrays': True
             }
         }
-
     ])
     .then(results => {
             res.status(200).json(results);
@@ -102,7 +101,7 @@ app.get('/movies/:Name', passport.authenticate('jwt', {session: false}), (req, r
  })
  .catch(err => {
   console.error(err);
-  res.status(500).send('Something broke!' + err);
+  res.status(500).send("Something broke!" + err);
  });
 });
 
